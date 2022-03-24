6ea9a736095422ce2b06c9e17546be8efb6c9f2f
Max Kollreider <max.kollreider@gmail.com>
2022-03-24 14:52:25 +0100

    WS2812FX support

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
+/*
+*/
+
 #include <Arduino.h>
 #include <ezButton.h>
+#include <WS2812FX.h>
 
 
 // Pindefinitions +++++++++++++++++
@@ -9,7 +13,11 @@
 #define Mos1 5   // Mosfet1
 #define Mos2 6   // Mosfet29
 
+#define ledPin 4
+#define numLed 60
+
 ezButton button(Button);
+WS2812FX ws2812fx = WS2812FX(numLed, ledPin, NEO_RGB + NEO_KHZ800);
 
 const int LONG_PRESS_TIME = 3000; // 3000 milliseconds (3 secs)
 bool released = true, state = false;
@@ -20,7 +28,14 @@ double volt, shift;
 void setup() {
   Serial.begin(9600);
   Serial.println();
+
   button.setDebounceTime(50);
+
+  ws2812fx.init();
+  ws2812fx.setBrightness(150);
+  ws2812fx.setSpeed(200);
+  ws2812fx.start();
+
   digitalWrite(Mos2, 0);        // start Headlight in off state
   digitalWrite(Mos1, 0);
 }
@@ -61,11 +76,11 @@ void loop() {
       analogWrite(Mos1, pwm);
     }
     else if(potValue < 540){    // off
-      analogWrite(Mos1, 0);
-      analogWrite(Mos2, 0);
+      digitalWrite(Mos1, 0);
+      digitalWrite(Mos2, 0);
     }
     else{
-      pwm = map(potValue, 540, 1024, 150, 255);
+      pwm = map(potValue, 540, 1024, 0, 255);
       analogWrite(Mos2, pwm);
     }
 
